b331c7251b01d8b9f41330b02f415b8412d3a88b
john0999 <moralesgomezjohnny@gmail.com>
2025-07-23 12:49:50 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/MantenedorController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AppSocial.Datos;
+using AppSocial.Models;
+namespace AppSocial.Controllers
+{
+    public class MantenedorController : Controller
+    {
+        ContactoDatos _ContactoDatos = new ContactoDatos();
+        public IActionResult Listar()
+        {
+            var oLista = _ContactoDatos.Listar();
+            return View(oLista);
+        }
+
+        public IActionResult Guardar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Guardar(ContactoModel oContacto)
+        {
+            var respuesta = _ContactoDatos.Guardar(oContacto);
+            if (respuesta)
+                return RedirectToAction("listar");
+            else
+                return View();
+        }
+    }
+}
